6ba24b049d28088e004198545c6b775cac6c3a7e
etboi1 <et.grundvig@gmail.com>
2024-09-14 21:31:03 -0600

    implemented code so that I'm now passing the KingMovesTest as wwell

--- shared/src/main/java/chess/KingMovesCalculator.java
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -1,11 +1,40 @@
 package chess;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 
 public class KingMovesCalculator implements PieceMovesCalculator{
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position, ChessPiece piece) {
-        return List.of();
+        Collection<ChessMove> moves = new HashSet<>();
+
+        int[] directionX = {-1, -1, -1, 0, 0, 1, 1, 1}; //A king can move diagonally up/down left, up/down, & diagonally up/down right.
+        int[] directionY = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+        for (int i = 0; i < directionX.length; i++) {
+            int col = position.getColumn();
+            int row = position.getRow();
+
+            col += directionX[i];
+            row += directionY[i];
+
+            if (1 <= row && row <= 8 && 1 <= col && col <= 8 ) {
+                ChessPosition newPosition = new ChessPosition(row, col);
+                ChessPiece checkSquare = board.getPiece(newPosition);
+
+                if (checkSquare != null) {
+                    if (checkSquare.getTeamColor() != piece.getTeamColor()) {
+                        //Can capture enemy piece
+                        moves.add(new ChessMove(position, newPosition, null));
+                    }
+                }
+                else {
+                    moves.add(new ChessMove(position, newPosition, null));
+                }
+            }
+        }
+
+        return moves;
     }
 }

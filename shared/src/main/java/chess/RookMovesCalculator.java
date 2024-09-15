483f1a24d418aae3ca9fa105803adf2198a2c777
etboi1 <et.grundvig@gmail.com>
2024-09-14 21:36:31 -0600

    Wrote and passed the tests for the queen moves and rook moves (basically the same as the bishop

--- shared/src/main/java/chess/RookMovesCalculator.java
+++ shared/src/main/java/chess/RookMovesCalculator.java
@@ -1,11 +1,48 @@
 package chess;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 
 public class RookMovesCalculator implements PieceMovesCalculator{
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position, ChessPiece piece) {
-        return List.of();
+        Collection<ChessMove> moves = new HashSet<>();
+
+        int[] directionX = {-1, 0, 0 , 1}; //Rooks can move in a straight line in any horizontal/vertical direction
+        int[] directionY = {0, -1, 1, 0};
+
+        for (int i = 0; i < directionX.length; i++) {
+            int col = position.getColumn();
+            int row = position.getRow();
+
+            while (true) {
+                col += directionX[i];
+                row += directionY[i];
+
+                if (col < 1 || col > 8 || row < 1 || row > 8) {
+                    break;
+                }
+
+                ChessPosition newPosition = new ChessPosition(row, col);
+                ChessPiece checkSquare = board.getPiece(newPosition);
+
+                //Check what's in the square we're trying to move to
+                if (checkSquare != null) {
+                    //If the piece is an enemy piece, it's still a valid move because we can capture it
+                    if (checkSquare.getTeamColor() != piece.getTeamColor()) {
+                        moves.add(new ChessMove(position, newPosition, null));
+                    }
+                    //If there was a piece, whoever it belonged to, we can't move past it
+                    break;
+                }
+                else {
+                    //If the square is empty, we add it and keep going
+                    moves.add(new ChessMove(position, newPosition, null));
+                }
+            }
+        }
+
+        return moves;
     }
 }

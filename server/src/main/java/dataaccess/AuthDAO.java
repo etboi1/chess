deaa41e0da57ca5f20750388fdd91e6673c0c052
etboi1 <et.grundvig@gmail.com>
2024-10-18 15:18:43 -0600

    Created AuthDAO, GameDAO, and UserDAO. I'm not 100% sure that the parameters on these methods are correct right now

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,4 +1,10 @@
 package dataaccess;
 
+import model.AuthData;
+
 public interface AuthDAO {
+    void clear();
+    void createAuth(AuthData authData);
+    AuthData getAuth(AuthData authData);
+    void deleteAuth(AuthData authData);
 }
--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,4 +1,11 @@
 package dataaccess;
 
+import model.GameData;
+
 public interface GameDAO {
+    void clear();
+    void createGame(String gameName);
+    GameData getGame(String gameID);
+    GameData[] listGames();
+    void joinGame(String gameID, GameData gameUpdate);
 }

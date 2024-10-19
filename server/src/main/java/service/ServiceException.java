b44cbbd5016da0a5b392dffe0ec7ba3ed8a8babc
etboi1 <et.grundvig@gmail.com>
2024-10-19 14:59:50 -0600

    Added the ServiceException and UserServices classes to begin implementing RegisterUser method and just to kinda get a feel for what's going on by following along with what Professor Jenson wrote in class

--- server/src/main/java/service/ServiceException.java
+++ server/src/main/java/service/ServiceException.java
@@ -1,4 +1,5 @@
 package service;
 
-public class ServiceException {
+public class ServiceException extends Exception {
+    public ServiceException(String message) {super(message);}
 }
--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,4 +1,18 @@
 package service;
 
+import dataaccess.UserDAO;
+import model.UserData;
+
 public class UserService {
+    private final UserDAO userDataAccess;
+
+    public UserService(UserDAO userDataAccess) {
+        this.userDataAccess = userDataAccess;
+    }
+
+    public UserData registerUser(UserData newUser) throws ServiceException {
+        if (userDataAccess.getUser(newUser.username()) != null) {
+            throw new ServiceException("User already exists");
+        }
+    }
 }

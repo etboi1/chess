b3daef3b2749c11a2640e96aea1eeff8eaac9f55
etboi1 <et.grundvig@gmail.com>
2024-10-19 14:57:11 -0600

    As I implemented registerUser on the UserService, I realized that I needed to change what was being passed into the getUser method on UserDAO - it just takes String username now

--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -5,5 +5,5 @@ import model.UserData;
 public interface UserDAO {
     void clear();
     void createUser(UserData userData);
-    UserData getUser(UserData userData);
+    UserData getUser(String username);
 }

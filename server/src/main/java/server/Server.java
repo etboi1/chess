901524f7a941bbdfdeb92d538c2edb2fbca35bed
etboi1 <et.grundvig@gmail.com>
2024-10-19 14:58:02 -0600

    added the code that Professor Jenson showed us how to write in class

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,8 +1,20 @@
 package server;
 
+import com.google.gson.Gson;
+import dataaccess.MemoryUserDAO;
+import dataaccess.UserDAO;
+import model.UserData;
+import org.eclipse.jetty.server.Authentication;
+import service.ServiceException;
+import service.UserService;
 import spark.*;
 
+import java.util.Map;
+
 public class Server {
+    private final UserDAO userDataAccess = new MemoryUserDAO();
+    private final UserService userService = new UserService(userDataAccess);
+    private final Gson serializer = new Gson();
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
@@ -10,12 +22,26 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.post("/user", this::createUser);
+        Spark.exception(Exception.class, this::exceptionHandler);
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
 
         Spark.awaitInitialization();
         return Spark.port();
+    }
+
+    private void exceptionHandler(Exception ex, Request req, Response res) {
+        res.status(500);
+        res.body(serializer.toJson(Map.of("message", ex.getMessage())));
+        ex.printStackTrace(System.out);
+    }
+
+    private String createUser(Request req, Response res) throws Exception {
+        var newUser = serializer.fromJson(req.body(), UserData.class);
+        var result = userService.registerUser(newUser);
+        return serializer.toJson(result);
     }
 
     public void stop() {

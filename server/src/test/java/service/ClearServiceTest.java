dff9ad85af70fd71cd74a45586915cd4ebe3674e
etboi1 <et.grundvig@gmail.com>
2024-10-20 01:11:05 -0600

    Neither of this tests are written correctly. I'm having trouble figuring out how to write them. I'm going to stop implementing my server until I can figure out how to do this, becuase I know it's a good skill to learn

--- server/src/test/java/service/ClearServiceTest.java
+++ server/src/test/java/service/ClearServiceTest.java
@@ -1,4 +1,31 @@
 package service;
 
+import dataaccess.*;
+import model.UserData;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class ClearServiceTest {
+    static private UserDAO userDataAccess;
+    static private AuthDAO authDataAccess;
+    static private GameDAO gameDataAccess;
+    static private ClearService clearService;
+    static private UserService userService;
+
+    @BeforeAll
+    public static void init() {
+        userDataAccess = new MemoryUserDAO();
+        authDataAccess = new MemoryAuthDAO();
+        gameDataAccess = new MemoryGameDAO();
+        clearService = new ClearService(userDataAccess, authDataAccess, gameDataAccess);
+        userService = new UserService(userDataAccess, authDataAccess);
+    }
+
+    @Test
+    public void clearData() throws Exception{
+        UserData user = new UserData("tusername", "tpassword", "temail");
+        userService.registerUser(user);
+        clearService.clearData();
+    }
 }
--- server/src/test/java/service/RegisterUserTest.java
+++ server/src/test/java/service/RegisterUserTest.java
@@ -1,4 +1,44 @@
 package service;
 
+import dataaccess.*;
+import model.UserData;
+import org.junit.jupiter.api.*;
+import response.LoginRegisterResponse;
+
 public class RegisterUserTest {
+    static private UserDAO userDataAccess;
+    static private AuthDAO authDataAccess;
+    static private GameDAO gameDataAccess;
+    static private UserService userService;
+    static private ClearService clearService;
+
+    @BeforeAll
+    public static void init() throws Exception {
+        userDataAccess = new MemoryUserDAO();
+        authDataAccess = new MemoryAuthDAO();
+        gameDataAccess = new MemoryGameDAO();
+        userService = new UserService(userDataAccess, authDataAccess);
+        clearService = new ClearService(userDataAccess, authDataAccess, gameDataAccess);
+        clearService.clearData();
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("Normal User Registration")
+    public void RegisterSuccess() throws Exception {
+        UserData user = new UserData("testUser", "testPassword", "testEmail");
+        var result = userService.registerUser(user);
+        Assertions.assertEquals(user, result);
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Registering an existing username")
+    public void RegisterFailure() throws Exception {
+        UserData user = new UserData("testUser", "testPassword", "testEmail");
+        Exception exception = Assertions.assertThrows(RedundantDataException.class, () -> {
+            userService.registerUser(user);
+        });
+        Assertions.assertEquals("Error: User already exists", exception.getMessage());
+    }
 }

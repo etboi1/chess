9777232caeaa35478a12bdaf6f12210200ba40eb
etboi1 <et.grundvig@gmail.com>
2024-10-20 14:36:27 -0600

    tweaked the interface to match the types I changed (listgames returns arraylist of gamedata and I changed the integer gameID on gamedata to a string

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -2,10 +2,12 @@ package dataaccess;
 
 import model.GameData;
 
+import java.util.ArrayList;
+
 public interface GameDAO {
     void clear();
-    void createGame(String gameName);
-    GameData getGame(String gameID);
-    GameData[] listGames();
+    void createGame(GameData newGame);
+    GameData getGame(String gameID) throws Exception;
+    ArrayList<GameData> listGames();
     void joinGame(String gameID, GameData gameUpdate);
 }

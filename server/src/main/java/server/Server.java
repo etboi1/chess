70ce44c7506398d44289061c82dac8d9bbd9b395
etboi1 <et.grundvig@gmail.com>
2024-10-20 23:43:35 -0600

    The server has been updated so that there is now a handler that at least exists for each endpoint, even if it hasn't been fully implemented yet. All endpoints are working except that JoinGame has not yet been implemented.

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -4,9 +4,11 @@ import com.google.gson.Gson;
 import dataaccess.*;
 import model.AuthData;
 import model.UserData;
+import request.CreateGameRequest;
 import service.*;
 import spark.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Server {
@@ -24,12 +26,15 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Check for authentication for game endpoints before allowing them to be handled
-
+        Spark.before("/game", this::authenticate);
 
         // Register your endpoints and handle exceptions here.
         Spark.post("/user", this::createUser);
         Spark.post("/session", this::loginUser);
         Spark.delete("/session", this::logoutUser);
+        Spark.get("/game", this::listGames);
+        Spark.post("/game", this::createGame);
+        Spark.put("/game", this::joinGame);
         Spark.delete("/db", this::clearDatabase);
         Spark.exception(Exception.class, this::exceptionHandler);
 
@@ -38,6 +43,22 @@ public class Server {
 
         Spark.awaitInitialization();
         return Spark.port();
+    }
+
+    private void authenticate(Request req, Response res) {
+        String authToken = req.headers("authorization");
+        if (authToken == null || authDataAccess.getAuth(new AuthData(authToken, null)) == null) {
+            Spark.halt(401, serializer.toJson(Map.of("message", "Error: unauthorized")));
+        }
+        Map<String, Object> reqBody;
+        if (req.body() == null || req.body().isEmpty()) {
+            reqBody = new HashMap<>();
+        } else {
+            reqBody = serializer.fromJson(req.body(), Map.class);
+        }
+        AuthData userAuth = authDataAccess.getAuth(new AuthData(authToken, null));
+        reqBody.put("username", userAuth.username());
+        serializer.toJson(reqBody);
     }
 
     private String createUser(Request req, Response res) throws Exception {
@@ -58,6 +79,21 @@ public class Server {
         userService.logoutUser(auth);
         res.status(200);
         return "";
+    }
+
+    private Object listGames(Request req, Response res) {
+        var result = gameService.listGames();
+        return serializer.toJson(result);
+    }
+
+    private Object createGame(Request req, Response res) {
+        CreateGameRequest createRequest = serializer.fromJson(req.body(), CreateGameRequest.class);
+        var result = gameService.createGame(createRequest);
+        return serializer.toJson(result);
+    }
+
+    private Object joinGame(Request request, Response response) {
+        return null;
     }
 
     private Object clearDatabase(Request req, Response res) throws Exception{

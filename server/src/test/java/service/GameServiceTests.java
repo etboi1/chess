961c1c23c1465084a483ac72e7b9dfd55b0ea84b
etboi1 <et.grundvig@gmail.com>
2024-10-22 17:42:00 -0600

    Added tests to test authorization with the new method I've been using. They are passing

--- server/src/test/java/service/GameServiceTests.java
+++ server/src/test/java/service/GameServiceTests.java
@@ -13,7 +13,6 @@ import response.ListGamesResponse;
 import java.util.ArrayList;
 
 public class GameServiceTests {
-    static private UserDAO userDataAccess;
     static private AuthDAO authDataAccess;
     static private GameDAO gameDataAccess;
     static private GameService gameService;
@@ -21,7 +20,7 @@ public class GameServiceTests {
 
     @BeforeAll
     public static void init() {
-        userDataAccess = new MemoryUserDAO();
+        UserDAO userDataAccess = new MemoryUserDAO();
         authDataAccess = new MemoryAuthDAO();
         gameDataAccess = new MemoryGameDAO();
         gameService = new GameService(gameDataAccess, authDataAccess);
@@ -58,6 +57,22 @@ public class GameServiceTests {
     }
 
     @Test
+    @DisplayName("List Games Failure - Unauthenticated User")
+    public void listFailure() {
+        // Add some games to the database
+        ChessGame game = new ChessGame();
+        GameData game1 = new GameData(1000, "white", "black", "00", game);
+        GameData game2 = new GameData(1001, "white", "black", "01", game);
+        gameDataAccess.createGame(game1);
+        gameDataAccess.createGame(game2);
+
+        // Call list games on server, but with an authToken that doesn't match
+        authDataAccess.createAuth(new AuthData("authToken", "username"));
+        Exception ex = Assertions.assertThrows(UnauthorizedException.class, () -> gameService.listGames("iDon'tMatch"));
+        Assertions.assertEquals("Error: unauthorized", ex.getMessage());
+    }
+
+    @Test
     @DisplayName("Successfully Create Game")
     public void createSuccess() throws Exception{
         authDataAccess.createAuth(new AuthData("authToken", "username"));
@@ -81,8 +96,20 @@ public class GameServiceTests {
     }
 
     @Test
+    @DisplayName("Create Game Failure - unauthorized user attempts to create game")
+    public void createFailure() throws Exception {
+        authDataAccess.createAuth(new AuthData("authToken", "username"));
+
+        // Attempt to create a game, but with an auth token that doesn't match
+        CreateGameRequest createRequest = new CreateGameRequest("gameName");
+        Exception ex = Assertions.assertThrows(UnauthorizedException.class, () -> gameService.createGame(createRequest, "badAuth"));
+        Assertions.assertEquals("Error: unauthorized", ex.getMessage());
+    }
+
+    @Test
     @DisplayName("Successfully Join a Game")
     public void joinSuccess() throws Exception {
+        // Manually create an authToken to be used in testing
         authDataAccess.createAuth(new AuthData("authToken", "testUser"));
         ChessGame game = new ChessGame();
 
@@ -107,15 +134,13 @@ public class GameServiceTests {
 
     @Test
     @DisplayName("Attempt to join a full game")
-    public void joinFailure() throws Exception {
+    public void joinFailure() {
         authDataAccess.createAuth(new AuthData("authToken", "username"));
 
         ChessGame game = new ChessGame();
         gameDataAccess.createGame(new GameData(1000, "white", "black", "name", game));
 
-        Exception ex = Assertions.assertThrows(RedundantDataException.class, () -> {
-            gameService.joinGame(new JoinGameRequest("WHITE", 1000), "authToken");
-        });
+        Exception ex = Assertions.assertThrows(RedundantDataException.class, () -> gameService.joinGame(new JoinGameRequest("WHITE", 1000), "authToken"));
         Assertions.assertEquals("Error: already taken", ex.getMessage());
     }
 }

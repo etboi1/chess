d3a674589e5f564f9053bdeac22c922764c84302
etboi1 <et.grundvig@gmail.com>
2024-10-22 17:52:58 -0600

    Wrote the final service test (which was for AuthService, which I just wrote here at the end) and it passes!

---
+++ server/src/test/java/service/AuthServiceTest.java
@@ -0,0 +1,50 @@
+package service;
+
+import dataaccess.*;
+import model.AuthData;
+import org.junit.jupiter.api.*;
+
+public class AuthServiceTest {
+    static private AuthDAO authDataAccess;
+    static private GameDAO gameDataAccess;
+    static private AuthService authService;
+    static private ClearService clearService;
+
+    @BeforeAll
+    public static void init() {
+        UserDAO userDataAccess = new MemoryUserDAO();
+        authDataAccess = new MemoryAuthDAO();
+        gameDataAccess = new MemoryGameDAO();
+        authService = new AuthService(authDataAccess);
+        clearService = new ClearService(userDataAccess, authDataAccess, gameDataAccess);
+    }
+
+    @BeforeEach
+    public void clearState() throws DataAccessException {
+        clearService.clearData();
+    }
+
+    @Test
+    @DisplayName("Successful Authentication")
+    public void authSuccess() throws Exception {
+        String username = "realUser";
+        String authToken = "goodAuth";
+        authDataAccess.createAuth(new AuthData(authToken, username));
+        AuthData expectedAuth = authDataAccess.getAuth(authToken);
+
+        AuthData actualAuth = authService.authenticate(authToken);
+        Assertions.assertEquals(expectedAuth, actualAuth);
+    }
+
+    @Test
+    @DisplayName("Authentication Failure - user has incorrect authToken")
+    public void authFailure() throws Exception {
+        String username = "realUser";
+        String authToken = "goodAuth";
+        authDataAccess.createAuth(new AuthData(authToken, username));
+        AuthData expectedAuth = authDataAccess.getAuth(authToken);
+
+        Exception ex = Assertions.assertThrows(UnauthorizedException.class, () -> authService.authenticate("badAuth"));
+        Assertions.assertEquals("Error: unauthorized", ex.getMessage());
+    }
+}

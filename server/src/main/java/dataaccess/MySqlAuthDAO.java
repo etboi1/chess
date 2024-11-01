105a4170598dbb58d9a8839f3bf3b8f855dc2a29
etboi1 <et.grundvig@gmail.com>
2024-10-31 18:38:20 -0600

    created my sql data access classes

---
+++ server/src/main/java/dataaccess/MySqlAuthDAO.java
@@ -0,0 +1,25 @@
+package dataaccess;
+
+import model.AuthData;
+
+public class MySqlAuthDAO implements AuthDAO{
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public AuthData createAuth(AuthData authData) {
+        return null;
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) {
+        return null;
+    }
+
+    @Override
+    public void deleteAuth(AuthData authData) {
+
+    }
+}
---
+++ server/src/main/java/dataaccess/MySqlGameDAO.java
@@ -0,0 +1,32 @@
+package dataaccess;
+
+import model.GameData;
+
+import java.util.ArrayList;
+
+public class MySqlGameDAO implements GameDAO {
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void createGame(GameData newGame) {
+
+    }
+
+    @Override
+    public GameData getGame(Integer gameID) throws Exception {
+        return null;
+    }
+
+    @Override
+    public ArrayList<GameData> listGames() {
+        return null;
+    }
+
+    @Override
+    public void updateGame(Integer gameID, GameData gameUpdate) {
+
+    }
+}

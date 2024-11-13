4926861aa74f6993a52cf547f5ab728ac9c0dae7
etboi1 <et.grundvig@gmail.com>
2024-11-13 11:09:40 -0700

    Got list games working on the UI

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -2,9 +2,13 @@ package ui;
 
 import exception.ResponseException;
 import model.AuthData;
+import model.GameData;
+import response.CreateGameResponse;
+import response.ListGamesResponse;
 import response.LoginRegisterResponse;
 import server.ServerFacade;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ChessClient {
@@ -45,12 +49,12 @@ public class ChessClient {
                 LoginRegisterResponse loginRes = server.login(username, password);
                 currentAuth = new AuthData(loginRes.authToken(), loginRes.username());
                 state = State.LOGGEDIN;
-                return String.format("You signed in as %s.", currentAuth.username());
+                return String.format("You signed in as %s.\n", currentAuth.username());
             } catch (ResponseException ex) {
                 throw new ResponseException(400, ex.getMessage());
             }
         }
-        throw new ResponseException(400, "Expected: <USERNAME> <PASSWORD>");
+        throw new ResponseException(400, "Expected: <USERNAME> <PASSWORD>\n");
     }
 
     public String register(String... params) throws ResponseException {
@@ -62,31 +66,71 @@ public class ChessClient {
                 LoginRegisterResponse registerRes = server.register(username, password, email);
                 currentAuth = new AuthData(registerRes.authToken(), registerRes.username());
                 state = State.LOGGEDIN;
-                return String.format("You registered and signed in as %s.", currentAuth.username());
+                return String.format("You registered and signed in as %s.\n", currentAuth.username());
             } catch (ResponseException ex) {
                 throw new ResponseException(400, ex.getMessage());
             }
         }
-        throw new ResponseException(400, "Expected: <USERNAME> <PASSWORD> <EMAIL>");
+        throw new ResponseException(400, "Expected: <USERNAME> <PASSWORD> <EMAIL>\n");
     }
 
-    public String logout() {
+    public String logout() throws ResponseException {
+        assertLoggedIn("logout");
+        try {
+            server.logout(currentAuth.authToken());
+            state = State.LOGGEDOUT;
+            currentAuth = null;
+        } catch (ResponseException ex) {
+            throw new ResponseException(400, ex.getMessage());
+        }
+        return "You have been logged out.\n";
+    }
+
+    public String createGame(String... params) throws ResponseException {
+        assertLoggedIn("create a game");
+        if (params.length == 1) {
+            String gameName = params[0];
+            try {
+                CreateGameResponse createRes = server.createGame(currentAuth.authToken(), gameName);
+                return String.format("Created game with ID %s and name %s.\n", createRes.gameID(), gameName);
+            } catch (ResponseException ex) {
+                throw new ResponseException(400, ex.getMessage());
+            }
+        }
+        throw new ResponseException(400, "Expected: <NAME> (only one name accepted at a time).\n");
+    }
+
+    public String listGames() throws ResponseException {
+        assertLoggedIn("list games");
+        try {
+            ListGamesResponse listRes = server.listGames(currentAuth.authToken());
+            ArrayList<GameData> games = listRes.games();
+            String display = "";
+            for (GameData game : games) {
+                String black = "AVAILABLE";
+                String white = "AVAILABLE";
+                if (game.blackUsername() != null) {
+                    black = game.blackUsername();
+                }
+                if (game.whiteUsername() != null) {
+                    white = game.whiteUsername();
+                }
+                display += String.format("[%s] GameName: %s, White Player: %s, BlackPlayer: %s\n",
+                        game.gameID(), game.gameName(), white, black);
+            }
+            return display;
+        } catch (ResponseException ex) {
+            throw new ResponseException(400, ex.getMessage());
+        }
+    }
+
+    public String joinGame(String... params) throws ResponseException {
+        assertLoggedIn("join a game");
         return null;
     }
 
-    public String createGame(String... params) {
-        return null;
-    }
-
-    public String listGames() {
-        return null;
-    }
-
-    public String joinGame(String... params) {
-        return null;
-    }
-
-    public String observeGame(String... params) {
+    public String observeGame(String... params) throws ResponseException {
+        assertLoggedIn("observe a game");
         return null;
     }
 
@@ -110,9 +154,9 @@ public class ChessClient {
                 """;
     }
 
-    private void assertLoggedIn() throws Exception {
+    private void assertLoggedIn(String attemptedFunction) throws ResponseException {
         if (state == State.LOGGEDOUT) {
-            throw new ResponseException(400, "You must login");
+            throw new ResponseException(400, "You must login to " + attemptedFunction + ".\n");
         }
     }
 }
